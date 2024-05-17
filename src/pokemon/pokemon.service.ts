e9aec6dd75cf5ab4a62efb8c981d1c01341c00a7
Patricio Bonetto <patricio.bonetto@outlook.com>
2024-05-17 10:11:10 -0300

    Primer commit, ta grandote

---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,58 @@
+import { Model, isValidObjectId } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+
+@Injectable()
+export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    const pokemon = await this.pokemonModel.create(createPokemonDto);
+
+    return pokemon;
+  }
+
+  findAll() {
+    return `This action returns all pokemon`;
+  }
+
+  async findOne(id: string) {
+    if (!isValidObjectId(id)) {
+      throw new BadRequestException(`El id ${id} no es válido`);
+    }
+    const pokemon: Pokemon = await this.pokemonModel.findById(id);
+
+    return pokemon;
+  }
+
+  async update(id: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne(id);
+
+    if (!pokemon) {
+      throw new NotFoundException(`No se encontro el pokemon con id ${id}`);
+    }
+
+    await pokemon.updateOne(updatePokemonDto, { new: true });
+
+    return { ...pokemon.toJSON(), ...updatePokemonDto };
+  }
+
+  async remove(id: string) {
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+
+    if (deletedCount === 0)
+      throw new NotFoundException(`Pokemon con id ${id} no se encontro`);
+
+    return;
+  }
+}
